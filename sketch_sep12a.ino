5b733f7685653eb45aee980794022954d65253b3
ridhon <ridhonainggolan6@gmail.com>
2016-09-12 22:57:02 +0700

    Add files via upload

---
+++ sketch_sep12a.ino
@@ -0,0 +1,29 @@
+void setup()
+{
+  pinMode(A0, INPUT);
+  digitalWrite(A0, HIGH);
+  pinMode(A1, INPUT);
+  digitalWrite(A1, HIGH);
+  pinMode(A2, INPUT);
+  digitalWrite(A2, HIGH);
+}
+
+void loop()
+{
+  if(digitalRead(A0)==LOW)
+  {
+    PORTB +=1;
+    delay(500);
+  }
+  
+  if(digitalRead(A1)==LOW)
+  {
+    PORTB -=1;
+    delay(500);
+  }
+  
+  if(digitalRead(A2)==LOW)
+  {
+    PORTB=0;
+  }
+}
